c82d00e7d4dc6d1e2a30254742e67524b72bbda9
nikaveli <85408770+nikaveli@users.noreply.github.com>
2022-02-11 22:13:46 -0700

    client upload

---
+++ src/styles.js
@@ -0,0 +1,14 @@
+import { makeStyles } from "@material-ui/core";
+
+export default makeStyles(() => ({
+    appBar: {
+      borderRadius: 15,
+      margin: '30px 0',
+      
+    },
+    heading: {
+      color: '#cc3333',
+    },
+    
+  }));
+  
